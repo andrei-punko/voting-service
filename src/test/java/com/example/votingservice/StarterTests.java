c1dcfee29b1ca96c9727d0ed62961c1e4c7d4ada
Andrei Punko <andd3dfx@gmail.com>
2021-11-06 14:41:20 +0300

    Adjust exclusion for tests. Add assert into test about context load

--- src/test/java/com/example/votingservice/StarterTests.java
+++ src/test/java/com/example/votingservice/StarterTests.java
@@ -1,13 +1,15 @@
 package com.example.votingservice;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StarterTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 }
